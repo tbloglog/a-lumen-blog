3c593b8c1c6f3102261d48b508e3653dc802db27
Francesco Guidi <francescoguidi@fattureincloud.it>
2020-10-05 11:33:27 +0200

    added custom exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,14 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -47,8 +52,24 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
-    public function render($request, Throwable $exception)
+    public function render($request, Throwable $e)
     {
-        return parent::render($request, $exception);
+
+        $exception_type = get_class($e);
+
+        switch($exception_type){
+            case NotFoundHttpException::class:
+                return response()->json(['title'=>['page not found']],404);
+
+            case MethodNotAllowedHttpException::class:
+                return response()->json(['title'=>['method not allowed']],405);
+
+            case ConflictHttpException::class:
+                return response()->json(['title'=>['conflict exception']],409);
+                
+            default:
+                return parent::render($request, $e);
+        }
+
     }
 }
